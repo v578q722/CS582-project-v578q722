4de3d2d2b049814254cd86ee0b357a495c94e051
v578q722 <130708573+v578q722@users.noreply.github.com>
2023-05-02 17:27:57 -0500

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -394,3 +394,19 @@ class Migration(migrations.Migration):
             preserve_default=False,
         ),
     ]
+
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('products', '0002_product_featured'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='product',
+            name='summary',
+            field=models.TextField(blank=True),
+        ),
+    ]
