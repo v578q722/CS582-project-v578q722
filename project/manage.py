fa722eda9c7f3add758a692bf51d180ec84f7880
v578q722 <130708573+v578q722@users.noreply.github.com>
2023-05-02 17:29:22 -0500

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -425,3 +425,20 @@ class Migration(migrations.Migration):
             field=models.TextField(),
         ),
     ]
+
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('products', '0004_auto_20180702_2213'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='product',
+            name='featured',
+            field=models.BooleanField(default=False),
+        ),
+    ]
+
