b42e66956cc41e727d95be92878cd2f98873e254
v578q722 <130708573+v578q722@users.noreply.github.com>
2023-05-02 17:27:07 -0500

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -377,3 +377,20 @@ class Migration(migrations.Migration):
             ],
         ),
     ]
+
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('products', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='product',
+            name='featured',
+            field=models.BooleanField(default=True),
+            preserve_default=False,
+        ),
+    ]
