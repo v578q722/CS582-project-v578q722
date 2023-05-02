30f67209b59f790aee67e0d23d616624326bde0f
v578q722 <130708573+v578q722@users.noreply.github.com>
2023-05-02 17:28:32 -0500

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -410,3 +410,18 @@ class Migration(migrations.Migration):
             field=models.TextField(blank=True),
         ),
     ]
+
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('products', '0003_auto_20180702_2213'),
+    ]
+    operations = [
+        migrations.AlterField(
+            model_name='product',
+            name='summary',
+            field=models.TextField(),
+        ),
+    ]
