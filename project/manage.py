d5c0d8ce087fcaf8d33105b9c9aa35fff507d756
v578q722 <130708573+v578q722@users.noreply.github.com>
2023-05-02 17:16:26 -0500

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -50,3 +50,17 @@ from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     name = 'blog'
+
+from django import forms
+
+from .models import Article
+
+class ArticleModelForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields =[
+            'title',
+            'content',
+            'active',
+        ]
+
