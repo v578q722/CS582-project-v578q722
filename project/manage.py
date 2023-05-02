ae97c22957406ed1cd0d0a1df7fee0496f5ad926
v578q722 <130708573+v578q722@users.noreply.github.com>
2023-05-02 17:19:13 -0500

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -137,3 +137,19 @@ class ArticleDeleteView(DeleteView):
 
     def get_success_url(self):
         return reverse('articles:article-list')
+    
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+    initial = True
+    dependencies = [
+    ]
+    operations = [
+        migrations.CreateModel(
+            name='Course',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=120)),
+            ],
+        ),
+    ]
