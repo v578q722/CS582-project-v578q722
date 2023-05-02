0ee99b372b38a29a71f3df4caef5a3d8c4834286
v578q722 <130708573+v578q722@users.noreply.github.com>
2023-05-02 17:25:18 -0500

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -291,3 +291,30 @@ class CourseCreateView(View):
             form = CourseModelForm()
         context = {"form": form}
         return render(request, self.template_name, context)
+
+class CourseListView(View):
+    template_name = "courses/course_list.html"
+    queryset = Course.objects.all()
+
+    def get_queryset(self):
+        return self.queryset
+
+    def get(self, request, *args, **kwargs):
+        context = {'object_list': self.get_queryset()}
+        return render(request, self.template_name, context)
+
+class CourseView(CourseObjectMixin, View):
+    template_name = "courses/course_detail.html" # DetailView
+    def get(self, request, id=None, *args, **kwargs):
+        # GET method
+        context = {'object': self.get_object()}
+        return render(request, self.template_name, context)
+
+    # def post(request, *args, **kwargs):
+    #     return render(request, 'about.html', {})
+
+# HTTP METHODS
+def my_fbv(request, *args, **kwargs):
+    print(request.method)
+    return render(request, 'about.html', {})
+
