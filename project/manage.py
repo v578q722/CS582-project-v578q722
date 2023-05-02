2f9111c231358d6bba20cbd040d3ec0fc1af5a4d
v578q722 <130708573+v578q722@users.noreply.github.com>
2023-05-02 17:17:36 -0500

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -75,3 +75,22 @@ class Article(models.Model):
     def get_absolute_url(self):
         return reverse("articles:article-detail", kwargs={"id": self.id})
 
+from django.test import TestCase
+
+from django.urls import path
+from .views import (
+    ArticleCreateView,
+    ArticleDeleteView,
+    ArticleDetailView,
+    ArticleListView,
+    ArticleUpdateView,
+)
+
+app_name = 'articles'
+urlpatterns = [
+    path('', ArticleListView.as_view(), name='article-list'),
+    path('create/', ArticleCreateView.as_view(), name='article-create'),
+    path('<int:id>/', ArticleDetailView.as_view(), name='article-detail'),
+    path('<int:id>/update/', ArticleUpdateView.as_view(), name='article-update'),
+    path('<int:id>/delete/', ArticleDeleteView.as_view(), name='article-delete'),
+]
