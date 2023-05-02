7e7f14c0d6f293042f0ddb316ae43f3a76881e82
v578q722 <130708573+v578q722@users.noreply.github.com>
2023-05-02 17:20:01 -0500

    Update manage.py

--- project/manage.py
+++ project/manage.py
@@ -160,3 +160,21 @@ from django.apps import AppConfig
 
 class CoursesConfig(AppConfig):
     name = 'courses'
+
+from django import forms
+
+from .models import Course
+
+class CourseModelForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = [
+            'title'
+        ]
+    
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if title.lower() == 'abc':
+            raise forms.ValidationError("This is not a valid title")
+        return title
+    
